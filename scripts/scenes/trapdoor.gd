8f78a7f193bbef346e7fe8c8b423abc32007dd40
Jagholin <pavel.belskiy2@gmail.com>
2024-03-24 19:06:15 +0100

    fix: Trapdoor now drops the player if they were standing on it when it opened

--- scripts/scenes/trapdoor.gd
+++ scripts/scenes/trapdoor.gd
@@ -5,9 +5,12 @@ extends Node3D
 @export var teleporter: Teleporter
 @export_group("Internals")
 @export var animation_tree: AnimationTree
+@export var player_tracker: PlayerTrackerComponent
 
 var is_transitioning: bool = false
 var is_now_open: bool = false
+
+var anim_started_was_called: bool = false
 
 func open():
 	animation_tree["parameters/conditions/open"] = true
@@ -17,16 +20,30 @@ func close():
 	animation_tree["parameters/conditions/open"] = false
 	animation_tree["parameters/conditions/closed"] = true
 
+func initiate_fall():
+	var chr := Globals.get_character_controller()
+	var cmp := chr.get_component(GridMovementComponent.GM_COMPONENT_NAME) as GridMovementComponent
+	cmp.apply_drop_down(teleporter)
+
 func _on_animation_tree_animation_started(anim_name):
+	# bug: AnimationTree will trigger animation_started signal twice in a row.
+	# if it just happened we have to ignore it because some of our code isn't reentrant
+	if anim_started_was_called:
+		return
+	anim_started_was_called = true
+	
 	if anim_name == "open_doors":
 		is_transitioning = true
 		is_now_open = true
+		if player_tracker.player_is_ontop:
+			initiate_fall()
 	elif anim_name == "close_doors":
 		is_transitioning = true
 	else:
 		assert(false, "Unknown animation name: " + anim_name)
 
 func _on_animation_tree_animation_finished(anim_name):
+	anim_started_was_called = false
 	if anim_name == "open_doors":
 		is_transitioning = false
 	elif anim_name == "close_doors":
@@ -37,6 +54,4 @@ func _on_animation_tree_animation_finished(anim_name):
 
 func on_player_is_ontop():
 	if is_now_open:
-		var chr := Globals.get_character_controller()
-		var cmp := chr.get_component(GridMovementComponent.GM_COMPONENT_NAME) as GridMovementComponent
-		cmp.apply_drop_down(teleporter)
+		initiate_fall()
