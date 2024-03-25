42877e68c4c8b17e724bbad484ac72781e11d6f9
Jagholin <pavel.belskiy2@gmail.com>
2024-03-25 12:50:36 +0100

    rename game_level to test_level

--- scripts/scenes/app_main.gd
+++ scripts/scenes/app_main.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var ui_layer: CanvasLayer
 @export var game_level: PackedScene
+@export var test_level: PackedScene
 
 var level: Level
 
@@ -10,7 +11,17 @@ func _on_exit_button_pressed():
 	get_tree().quit()
 
 func _on_start_button_pressed():
+	if not game_level:
+		return
 	var myLevel := game_level.instantiate()
+	add_child(myLevel)
+	level = myLevel
+	ui_layer.hide()
+
+func _on_start_test_level_button_pressed():
+	if not test_level:
+		return
+	var myLevel := test_level.instantiate()
 	add_child(myLevel)
 	level = myLevel
 	ui_layer.hide()
